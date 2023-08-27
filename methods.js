44be1bbdd42e6fc8e7f67ae7f24cde421f8a9e1b
shimon <123260188+AvarDysania@users.noreply.github.com>
2023-08-27 11:50:34 +0530

    added methods.js

---
+++ methods.js
@@ -0,0 +1,17 @@
+function github()
+{
+    window.location.href="https://github.com/AvarDysania";
+    
+}
+function linkedln()
+{
+    window.location.href="https://www.linkedin.com/in/shimon-joel-63097b229";
+}
+function facebook()
+{
+    window.location.href="https://www.facebook.com/shimon.joel.9";
+}
+function telegram()
+{
+    window.location.href="https://t.me/Shimon";
+}
